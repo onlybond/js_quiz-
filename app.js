743830c3679fb44f8e5ba25ce9d8615ac891695a
onlybond <kevalsudra777@gmail.com>
2022-08-20 00:48:52 +0530

    modified:   app.js 	modified:   index.html 	modified:   style.css

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 var myQuestions = [
 	{
-		question: "What is 10/2?",
+		question: "Why is stackup the best platform to learn and earn?",
 		answers: {
-			a: '3',
-			b: '5',
-			c: '115'
-		},
-		correctAnswer: 'b'
-	},
-	{
-		question: "What is 30/3?",
-		answers: {
-			a: '3',
-			b: '5',
-			c: '10'
+			a: 'because of the stackup discord community',
+			b: 'because of their pretty and hardworking CM Aunt bern ',
+			c: 'All of the above'
 		},
 		correctAnswer: 'c'
-	}
+	},
+	{
+		question: "if you are travelling a a speed of 40 km/h and\n accelerating at a rate of 3km.h. and the\n gravity is 10g, what is\n the probability you will survive bern's slipper attack",
+		answers: {
+			a: 'spot ded',
+			b: 'dodge [never an option, still to be fair kek]',
+			c: 'will do cpr yourself'
+		},
+		correctAnswer: 'a'
+	},
+	{
+	question: "what did i do before finding stackup",
+		answers: {
+			a: 'nothing',
+			b: 'nothing',
+			c: 'nothing'
+		},
+		correctAnswer: 'a'
+	},
 ];
 
 var quizContainer = document.getElementById('quiz');
